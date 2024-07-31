966974c71494c5ee47252714a8e81de4bebb2a1e
caio1020ee <caiolucasamorim@gmail.com>
2024-07-31 19:50:28 -0300

    apitavaerrado

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://homologacao3.azapfy.com.br/api/ps/',
+  baseURL: '//homologacao3.azapfy.com.br/api/ps/metahumans',
 });
 
 export default api;
